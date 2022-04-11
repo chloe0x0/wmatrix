7dd1593bca18d32da4b868d08033edc2f6418b97
chloe0b0 <chloezeiter@gmail.com>
2022-04-10 18:16:06 -0700

    Removed unused paramaters

--- src/matrix.c
+++ src/matrix.c
@@ -11,14 +11,14 @@
 #define ACTIVE_P        0.0005  // Probability that an inactive cell will become active
 #define FADE_P          0.002   // Probability that an active cell will become inactive
 #define STATE_PHASE     0.9     // Probability that a cell will randomly change its state
-#define MAX_LIFESPAN    6       // Max number of iterations a cell can stay active
+#define MAX_LIFESPAN    5       // Max number of iterations a cell can stay active
 #define INIT_ACTIVE_P   0.01    // Probability that a cell will be active upon initialization
-#define SLEEP_MS        25      // Time in MS to sleep after printing matrix
+#define SLEEP_MS        75      // Time in MS to sleep after printing matrix
 #define WIDTH           100      // Width of the matrix in Cells      
 #define HEIGHT          50      // Height of the matrix in Cells
 
 #define WHITE 7
-#define GREEN 2
+#define GREEN 2  // 207
 
 static HANDLE handle;
 static WORD   Attrs;
@@ -29,7 +29,7 @@ void ResetConsole(){
 }
 
 char RandChar(){
-    return 'A' + (rand() % 25);
+    return 'A' + (rand() % 100);
 }
 
 WORD RandColor(){
@@ -54,7 +54,7 @@ Node* InitMatrix(unsigned int cells, COORD InitPos){
     return matrix;
 }
 
-Node* Simulate(Node* Matrix, unsigned int cells, unsigned int width){
+Node* Simulate(Node* Matrix, unsigned int cells){
     Node* NewMatrix = malloc( sizeof(Node) * cells );
     memcpy(NewMatrix, Matrix, sizeof(Node) * cells);
 
@@ -68,7 +68,7 @@ Node* Simulate(Node* Matrix, unsigned int cells, unsigned int width){
             NewMatrix[i].active = (bool)!(++NewMatrix[i].lifespan == MAX_LIFESPAN);
             NewMatrix[i].lifespan *= NewMatrix[i].active;
 
-            if (i + width < cells){ NewMatrix[i + width].active = true; }
+            if (i + WIDTH < cells){ NewMatrix[i + WIDTH].active = true; }
         }else{
             if (p < ACTIVE_P){
                 NewMatrix[i].active = true;
@@ -81,11 +81,11 @@ Node* Simulate(Node* Matrix, unsigned int cells, unsigned int width){
     return NewMatrix;
 }
 
-void DisplayMatrix(Node* Matrix, unsigned int cells, unsigned int width, WORD* ColorArray){
+void DisplayMatrix(Node* Matrix, unsigned int cells){
     char* matrix = malloc( sizeof(char)*cells + HEIGHT + 1);
 
     for (int i = 0; i < cells; ++i){
-        if ((i + 1) % width == 0){ matrix[i] = '\n'; continue; }
+        if ((i + 1) % WIDTH == 0){ matrix[i] = '\n'; continue; }
 
         Node n = Matrix[i];
         matrix[i] = n.active ? n.state : ' ';
@@ -111,18 +111,12 @@ int main(void){
     unsigned int cells = WIDTH * HEIGHT;
 
     COORD InitPos = csbi.dwCursorPosition;
-
-    WORD* Colors = calloc(MAX_LIFESPAN, sizeof(WORD));
-    Colors[0] = 0x0;
-    Colors[1] = WHITE;
-    for (int i = 2; i < MAX_LIFESPAN; ++i){ Colors[i] = GREEN; }
-
     Node* matrix = InitMatrix(cells, InitPos);
 
     SetConsoleTextAttribute(handle, GREEN);
     while (true){
-        matrix = Simulate(matrix, cells, WIDTH);
-        DisplayMatrix(matrix, cells, WIDTH, Colors);
+        matrix = Simulate(matrix, cells);
+        DisplayMatrix(matrix, cells);
         SetConsoleCursorPosition(handle, InitPos);
         Sleep(SLEEP_MS);
     }
